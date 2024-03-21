e417d858dc84c8468ccc84e443ed2a5bcaf52676
vixidurr2402 <chien27122402@gmail.com>
2024-03-21 10:23:44 +0700

    baitapcsharp

---
+++ Buổi 2/ProjectFirst/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; // đây là thư viện có thể gọi ra những kiểu viết dấu như Unicode, UTF8 các hàm OutputEncoding
+using System.Threading.Tasks;
+
+namespace ProjectFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.OutputEncoding = Encoding.Unicode; // để có thể  in ra Tiếng Việt có dấu
+            Console.WriteLine("Van Chien");
+            Console.WriteLine("Xin chào! Van Chien");
+            // test có dấu 
+            Console.WriteLine("Xin chào test Trần Văn Chiến");
+            //Console.ReadLine();
+            // Có thể dùng ReadKey(); để ngắt chương trình 
+            Console.ReadKey(); // dùng để ngắt chương trình  
+            */
+            Console.OutputEncoding = Encoding.Unicode;
+            int a;
+            int b;
+
+            Console.WriteLine("Nhap a = "); a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap b = "); b = int.Parse(Console.ReadLine());
+            double x = -b / a;
+            Console.WriteLine("In ra x = {0}", x);
+            Console.WriteLine("In ra a, b, x: {0} {1} {2}", a, b, x);
+            Console.ReadKey ();
+            //if (a == 0)
+            //{
+            //    Console.WriteLine("Vô số nghiệm!\n");
+            //}
+        }
+    }
+}
---
+++ Buổi 2/Project_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            float x, y;
+            Console.Write("Nhap x = "); x = float.Parse(Console.ReadLine());
+            Console.Write("Nhap y = "); y = float.Parse(Console.ReadLine());
+            float Sum = x/y;
+            Console.WriteLine("Tong la: {0}", Sum);
+            Console.ReadKey(); 
+        }
+    }
+}
